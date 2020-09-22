1ae133253bb3ac7d2a61115c9762bf3da5c85a77
guotianci1997 <xiaorubaby333@163.com>
2020-09-22 11:01:11 +0800

    提交home-head

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import store from './store/index.js'
+import Swiper,{Navigation,Pagination,Autoplay} from 'swiper'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+Swiper.use([Navigation,Pagination,Autoplay])
+Vue.use(VueAwesomeSwiper)
+import 'swiper/swiper-bundle.css'
 
 Vue.config.productionTip = false
 
